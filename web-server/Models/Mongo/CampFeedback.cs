4cdd2ace3b9326b86fa659488ecd1fd2d58f20af
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-09-03 21:55:22 +0530

    feat: create CampFeedback.cs

---
+++ web-server/Models/Mongo/CampFeedback.cs
@@ -0,0 +1,27 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace web_server.Models.Mongo;
+
+public class CampFeedback
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("campId")]
+    public int CampId { get; set; }
+
+    [BsonElement("donorId")]
+    public int DonorId { get; set; }
+
+    [BsonElement("rating")]
+    public int Rating { get; set; } // 1 to 5
+
+    [BsonElement("comments")]
+    public string Comments { get; set; } = string.Empty;
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; }
+}
