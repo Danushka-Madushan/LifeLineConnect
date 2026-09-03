0f8dfda88daf73aa69dbcb88c3e22f3e0ca6c3b0
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-09-03 23:29:59 +0530

    update Home.tsx

--- web-ui/src/pages/Home.tsx
+++ web-ui/src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+
 import { Link } from 'react-router-dom';
 import { api } from '../lib/api';
 
@@ -207,3 +208,4 @@ const Home = () => {
 };
 
 export default Home;
+
