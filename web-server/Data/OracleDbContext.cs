f4585953cfc84aac474f5c816b351406000cdfee
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-09-03 21:55:01 +0530

    feat: create OracleDbContext.cs

---
+++ web-server/Data/OracleDbContext.cs
@@ -0,0 +1,21 @@
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using Microsoft.Extensions.Configuration;
+
+namespace web_server.Data
+{
+    public class OracleDbContext
+    {
+        private readonly string _connectionString;
+
+        public OracleDbContext(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("OracleDb") ?? "";
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new OracleConnection(_connectionString);
+        }
+    }
+}
