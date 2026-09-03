de92b22235e95b5b051e49343ce705522589ea9e
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-09-03 21:55:20 +0530

    feat: create AwarenessMaterial.cs

---
+++ web-server/Models/Mongo/AwarenessMaterial.cs
@@ -0,0 +1,33 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace web_server.Models.Mongo;
+
+public class AwarenessMaterial
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("title")]
+    public string Title { get; set; } = string.Empty;
+
+    [BsonElement("description")]
+    public string Description { get; set; } = string.Empty;
+
+    [BsonElement("mediaType")]
+    public string MediaType { get; set; } = "image"; // image, document
+
+    [BsonElement("url")]
+    public string Url { get; set; } = string.Empty;
+
+    [BsonElement("published")]
+    public bool Published { get; set; }
+
+    [BsonElement("campId")]
+    public int? CampId { get; set; }
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; }
+}
