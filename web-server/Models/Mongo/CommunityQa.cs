5268c93c379db80458986446d0c9b96789ba61fd
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-09-03 21:55:25 +0530

    feat: create CommunityQa.cs

---
+++ web-server/Models/Mongo/CommunityQa.cs
@@ -0,0 +1,31 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace web_server.Models.Mongo;
+
+public class CommunityQa
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("question")]
+    public string Question { get; set; } = string.Empty;
+
+    [BsonElement("answer")]
+    public string Answer { get; set; } = string.Empty;
+
+    [BsonElement("category")]
+    public string Category { get; set; } = string.Empty;
+
+    [BsonElement("tags")]
+    public List<string> Tags { get; set; } = new();
+
+    [BsonElement("helpfulCount")]
+    public int HelpfulCount { get; set; }
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; }
+}
