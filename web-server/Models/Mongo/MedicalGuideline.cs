3f0fd6bdc37e26916818044fccb871d970dd61b5
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-09-03 21:55:36 +0530

    feat: create MedicalGuideline.cs

---
+++ web-server/Models/Mongo/MedicalGuideline.cs
@@ -0,0 +1,24 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace web_server.Models.Mongo;
+
+public class MedicalGuideline
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("title")]
+    public string Title { get; set; } = string.Empty;
+
+    [BsonElement("content")]
+    public string Content { get; set; } = string.Empty;
+
+    [BsonElement("category")]
+    public string Category { get; set; } = string.Empty;
+
+    [BsonElement("lastUpdated")]
+    public DateTime LastUpdated { get; set; }
+}
