3ed58804b091b53ed6cceb0c12be17906ad1e497
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-09-03 23:30:01 +0530

    update Camps.tsx

--- web-ui/src/pages/Camps.tsx
+++ web-ui/src/pages/Camps.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+
 import { api } from '../lib/api';
 
 interface DonationCamp {
@@ -152,3 +153,4 @@ const Camps = () => {
 };
 
 export default Camps;
+
