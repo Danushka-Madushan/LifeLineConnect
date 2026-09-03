a5d680693079cd3920f75c489e140210d38eadf1
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-09-03 21:55:16 +0530

    feat: create SystemStats.cs

---
+++ web-server/Models/SystemStats.cs
@@ -0,0 +1,9 @@
+namespace web_server.Models
+{
+    public class SystemStats
+    {
+        public int TotalDonors { get; set; }
+        public int ActiveCamps { get; set; }
+        public double LitersCollected { get; set; }
+    }
+}
